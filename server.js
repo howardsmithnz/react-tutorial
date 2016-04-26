0d5442dfdeb155fc25a44a08b28977290e648961
Howard Smith <howardsmithnz@gmail.com>
2016-04-27 11:45:56 +1200

    change data source from props to state

--- server.js
+++ server.js
@@ -10,48 +10,48 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+var fs = require('fs')
+var path = require('path')
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
 
-var COMMENTS_FILE = path.join(__dirname, 'comments.json');
+var COMMENTS_FILE = path.join(__dirname, 'comments.json')
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3000))
 
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Additional middleware which will set headers that we need on each request.
-app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
+app.use(function (req, res, next) {
+  // Set permissive CORS header - this allows this server to be used only as
+  // an API server in conjunction with something like webpack-dev-server.
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
-    // Disable caching so we'll always get the latest comments.
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-});
+  // Disable caching so we'll always get the latest comments.
+  res.setHeader('Cache-Control', 'no-cache')
+  next()
+})
 
-app.get('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
+app.get('/api/comments', function (req, res) {
+  fs.readFile(COMMENTS_FILE, function (err, data) {
     if (err) {
-      console.error(err);
-      process.exit(1);
+      console.error(err)
+      process.exit(1)
     }
-    res.json(JSON.parse(data));
-  });
-});
+    res.json(JSON.parse(data))
+  })
+})
 
-app.post('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
+app.post('/api/comments', function (req, res) {
+  fs.readFile(COMMENTS_FILE, function (err, data) {
     if (err) {
-      console.error(err);
-      process.exit(1);
+      console.error(err)
+      process.exit(1)
     }
-    var comments = JSON.parse(data);
+    var comments = JSON.parse(data)
     // NOTE: In a real implementation, we would likely rely on a database or
     // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
     // treat Date.now() as unique-enough for our purposes.
@@ -59,19 +59,18 @@ app.post('/api/comments', function(req, res) {
       id: Date.now(),
       author: req.body.author,
       text: req.body.text,
-    };
-    comments.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+    }
+    comments.push(newComment)
+    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function (err) {
       if (err) {
-        console.error(err);
-        process.exit(1);
+        console.error(err)
+        process.exit(1)
       }
-      res.json(comments);
-    });
-  });
-});
+      res.json(comments)
+    })
+  })
+})
 
-
-app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
-});
+app.listen(app.get('port'), function () {
+  console.log('Server started: http://localhost:' + app.get('port') + '/')
+})
